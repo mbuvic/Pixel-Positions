877e9778acfd604a4226322cc200ad513fd885b4
mbuvic <charlesmbuvipeter@gmail.com>
2025-03-15 11:11:33 +0300

    Layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+            <div>
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+</html>
